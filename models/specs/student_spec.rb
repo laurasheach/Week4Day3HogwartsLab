95dc4d616f852be844f861772e1ed75797b6f2ff
Laura Sheach <laura_bubble_git@yahoo.com>
2019-05-29 10:51:45 +0100

    Part 1 complete of lab

---
+++ models/specs/student_spec.rb
@@ -0,0 +1,35 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../student.rb')
+
+class TestStudent < MiniTest::Test
+
+  def setup
+
+    options = {"id" => 1, "first_name" => "Harry", "last_name" => "Potter", "house" => "Gryffindor", "age" => 12}
+
+    @student = Student.new(options)
+
+  end
+
+  def test_id
+    assert_equal(1, @student.id)
+  end
+
+  def test_first_name
+    assert_equal("Harry", @student.first_name)
+  end
+
+  def test_last_name
+    assert_equal("Potter", @student.last_name)
+  end
+
+  def test_house
+    assert_equal("Gryffindor", @student.house)
+  end
+
+  def test_age
+    assert_equal(12, @student.age)
+  end
+
+end
---
+++ models/student.rb
@@ -0,0 +1,14 @@
+class Student
+
+  attr_reader :id, :first_name, :last_name, :house, :age
+
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @first_name = options["first_name"]
+    @last_name = options["last_name"]
+    @house = options["house"]
+    @age = options["age"].to_i
+  end
+
+end
